16bebc8dc96d9e08b833bde218a93f855675acb3
td-suz00 <b0651118@gmail.com>
2019-06-20 10:23:39 +0900

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,28 @@
+Rails.application.routes.draw do
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'
+  }
+  devise_scope :user do
+    get "sign_in", :to => "users/sessions#new"
+    get '/users/sign_out', :to => 'devise/sessions#destroy'
+  end
+
+  root 'images#index'
+
+  resources :images, except: :show do
+    resources :comments, only: [:new, :create]
+    resources :favorites, only: [:create, :destroy]
+
+    collection do
+      get :search
+    end
+
+  end
+
+  resources :users, only: :show do
+    resources :favorites, only: :index
+    resources :mypages, only: :index
+  end
+
+end
