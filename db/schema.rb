e75c8ba28ab1d550832f2461522266fd06102480
td-suz00 <b0651118@gmail.com>
2019-06-21 23:42:55 +0900

    update

--- db/schema.rb
+++ db/schema.rb
@@ -48,7 +48,7 @@ ActiveRecord::Schema.define(version: 2019_05_19_041021) do
 
   create_table "users", force: :cascade do |t|
     t.string "nickname", default: "", null: false
-    t.string "icon_image", default: ""
+    t.string "image", default: ""
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
