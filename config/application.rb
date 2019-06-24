b9f07d1499c78551895f66a7988df63f4d50f2e7
td-suz00 <b0651118@gmail.com>
2019-06-24 17:16:00 +0900

    update

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module Banshow
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
+    config.assets.initialize_on_precompile = false
     
 
     # Settings in config/environments/* take precedence over those specified here.
