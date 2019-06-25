21913cd1fb1102285ef20a89f883e4ae225d6d8a
td-suz00 <b0651118@gmail.com>
2019-06-25 10:14:26 +0900

    画像枠いっぱいに表示　マイページページネーション

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -2,6 +2,6 @@ class MypagesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @images = Image.all.where( user_id: current_user.id ).order("created_at DESC")
+    @images = Image.all.where( user_id: current_user.id ).order("created_at DESC").page(params[:page]).per(10)
   end
 end
