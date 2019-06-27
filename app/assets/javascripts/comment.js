36acb7d17e3bf5c9cec59cb2cc4dbb73db449caa
td-suz00 <b0651118@gmail.com>
2019-06-27 16:45:01 +0900

    fix comment.js

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,47 +1,92 @@
 $(function(){
   function buildMyComment(data){
-    var html = `
-                <div class = mine>
-                  <div class = my_info>
-                    <div class = my_image>
-                      <img src=${data.user_icon_image} class=icon_image>
+    if (data.user_icon_image != null) {
+      var html = `
+                  <div class = mine>
+                    <div class = my_info>
+                      <div class = my_image>
+                        <img src=${data.user_icon_image} class=icon_image>
+                      </div>
+                      <div class = my_name>
+                        ${data.user_nickname}
+                      </div>
                     </div>
-                    <div class = my_name>
-                      ${data.user_nickname}
-                    </div>
-                  </div>
 
-                  <div class = my_comment>
-                    ${data.text}
+                    <div class = my_comment>
+                      ${data.text}
+                    </div>
                   </div>
-                </div>
-                <div class = my_comment_time >
-                  ${data.created_at}
-                </div>
-              `
-    return html
+                  <div class = my_comment_time >
+                    ${data.created_at}
+                  </div>
+                `
+      return html
+    } else {
+      var html = `
+                  <div class = mine>
+                    <div class = my_info>
+                      <div class = my_image>
+                        <img src="no_avator.png" class=icon_image>
+                      </div>
+                      <div class = my_name>
+                        ${data.user_nickname}
+                      </div>
+                    </div>
+
+                    <div class = my_comment>
+                      ${data.text}
+                    </div>
+                  </div>
+                  <div class = my_comment_time >
+                    ${data.created_at}
+                  </div>
+                `
+      return html
+    }
   }
 
   function buildOtherComment(data){
-    var html = `
-                <div class = other>
-                  <div class = other_user_info>
-                    <div class = other_user_image>
-                    <img src=${data.user_icon_image} class=icon_image>
+    if (data.user_icon_image != null) {
+      var html = `
+                  <div class = other>
+                    <div class = other_user_info>
+                      <div class = other_user_image>
+                        <img src=${data.user_icon_image} class=icon_image>
+                      </div>
+                      <div class = other_user_name>
+                        ${data.user_nickname}
+                      </div>
                     </div>
-                    <div class = other_user_name>
-                      ${data.user_nickname}
+                    <div class = other_user_comment>
+                      ${data.text}
                     </div>
                   </div>
-                  <div class = other_user_comment>
-                    ${data.text}
+                  <div class = other_user_comment_time >
+                    ${data.created_at}
                   </div>
-                </div>
-                <div class = other_user_comment_time >
-                  ${data.created_at}
-                </div>
-              `
-    return html
+                `
+      return html
+    } else {
+      var html = `
+                  <div class = other>
+                    <div class = other_user_info>
+                      <div class = other_user_image>
+                        <img src="no_avator.png" class=icon_image>
+                      </div>
+                      <div class = other_user_name>
+                        ${data.user_nickname}
+                      </div>
+                    </div>
+                    <div class = other_user_comment>
+                      ${data.text}
+                    </div>
+                  </div>
+                  <div class = other_user_comment_time >
+                    ${data.created_at}
+                  </div>
+                `
+      return html
+    }
   }
 
 
@@ -61,6 +106,9 @@ $(function(){
 
     .done(function(data){
       // 送信者がimageのuserの場合と、それ以外で場合分け
+
+      console.log(data)
+
       if(data.comment_user_id==data.image_user_id){
         var html = buildMyComment(data);
         $('.comment').prepend(html)
--- app/views/comments/create.json.jbuilder
+++ app/views/comments/create.json.jbuilder
@@ -1,9 +1,5 @@
 json.text             @comment.text
-if @comment.user.icon_image.present?
-  json.user_icon_image  @comment.user.icon_image.url
-else
-  json.user_icon_image  "/app/assets/images/no_avator.png"
-end
+json.user_icon_image  @comment.user.icon_image.url
 json.user_nickname    @comment.user.nickname
 json.comment_user_id  @comment.user.id
 json.image_user_id    @comment.image.user.id
